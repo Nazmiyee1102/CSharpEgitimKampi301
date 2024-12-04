45cfce5ce88304136a232753da864fd556f26cb1
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2024-12-04 19:02:10 +0300

    #Dependency Injection İşlemleri

---
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace CSharpEgitimKampi301.PresentationLayer
+{
+    partial class FrmCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.btn_listele = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_categoryName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdb_active = new System.Windows.Forms.RadioButton();
+            this.rdb_passive = new System.Windows.Forms.RadioButton();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            this.btn_getir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(62, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kategori ID :";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txt_id.Location = new System.Drawing.Point(191, 63);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(172, 30);
+            this.txt_id.TabIndex = 1;
+            // 
+            // btn_listele
+            // 
+            this.btn_listele.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_listele.Location = new System.Drawing.Point(191, 235);
+            this.btn_listele.Name = "btn_listele";
+            this.btn_listele.Size = new System.Drawing.Size(172, 35);
+            this.btn_listele.TabIndex = 2;
+            this.btn_listele.Text = "Listele";
+            this.btn_listele.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(386, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(402, 431);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(52, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kategori Adı :";
+            // 
+            // txt_categoryName
+            // 
+            this.txt_categoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txt_categoryName.Location = new System.Drawing.Point(191, 119);
+            this.txt_categoryName.Name = "txt_categoryName";
+            this.txt_categoryName.Size = new System.Drawing.Size(172, 30);
+            this.txt_categoryName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(12, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kategori Durumu :";
+            // 
+            // rdb_active
+            // 
+            this.rdb_active.AutoSize = true;
+            this.rdb_active.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.rdb_active.Location = new System.Drawing.Point(191, 185);
+            this.rdb_active.Name = "rdb_active";
+            this.rdb_active.Size = new System.Drawing.Size(68, 29);
+            this.rdb_active.TabIndex = 4;
+            this.rdb_active.TabStop = true;
+            this.rdb_active.Text = "Aktif";
+            this.rdb_active.UseVisualStyleBackColor = true;
+            // 
+            // rdb_passive
+            // 
+            this.rdb_passive.AutoSize = true;
+            this.rdb_passive.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.rdb_passive.Location = new System.Drawing.Point(278, 185);
+            this.rdb_passive.Name = "rdb_passive";
+            this.rdb_passive.Size = new System.Drawing.Size(73, 29);
+            this.rdb_passive.TabIndex = 5;
+            this.rdb_passive.TabStop = true;
+            this.rdb_passive.Text = "Pasif";
+            this.rdb_passive.UseVisualStyleBackColor = true;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_ekle.Location = new System.Drawing.Point(191, 292);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(172, 35);
+            this.btn_ekle.TabIndex = 2;
+            this.btn_ekle.Text = "Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_sil.Location = new System.Drawing.Point(191, 345);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(172, 35);
+            this.btn_sil.TabIndex = 2;
+            this.btn_sil.Text = "Sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_guncelle.Location = new System.Drawing.Point(191, 403);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(172, 35);
+            this.btn_guncelle.TabIndex = 2;
+            this.btn_guncelle.Text = "Güncelle";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            // 
+            // btn_getir
+            // 
+            this.btn_getir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_getir.Location = new System.Drawing.Point(191, 459);
+            this.btn_getir.Name = "btn_getir";
+            this.btn_getir.Size = new System.Drawing.Size(172, 35);
+            this.btn_getir.TabIndex = 2;
+            this.btn_getir.Text = "ID\' ye Göre Getir";
+            this.btn_getir.UseVisualStyleBackColor = true;
+            // 
+            // FrmCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.rdb_passive);
+            this.Controls.Add(this.rdb_active);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_getir);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.btn_listele);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_categoryName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCategory";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Button btn_listele;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_categoryName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdb_active;
+        private System.Windows.Forms.RadioButton rdb_passive;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_guncelle;
+        private System.Windows.Forms.Button btn_getir;
+    }
+}
+
